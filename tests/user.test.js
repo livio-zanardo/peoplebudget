275be31ee2fdf8931a37fd71176792e7c599b822
Miguel Amaro <miguel.amaro001@mymdc.net>
2020-03-19 17:32:05 -0400

    Added userAPI test cases Create, Get One, Get All, Update(ecl. roles), Delete(arrays)

--- tests/user.test.js
+++ tests/user.test.js
@@ -16,25 +16,73 @@ const cleaningUpDatabase = async done => {
   await connection.end();
   done();
 };
-const jsonData = {
-	email: "jonDan26@gmail.com",
-	fname: "Johnny",
-	lname: "D'Antonino",
-	pass : "pass",
-	recover_pass: "fralalalala"
-}
-
+let testUserID = null;
+const testJsonData = {
+  email: "testUser001@email.com",
+  fname: "nameFirst",
+  lname: "nameLast",
+  pass: "password",
+  recover_pass: "securityResponse"
+};
+const testJsonDataResponse = {
+  id: "",
+  email: "testUser001@email.com",
+  firstName: "nameFirst",
+  lastName: "nameLast",
+  RoleId: null
+};
 describe("User CRUD API", () => {
   beforeAll(cleaningUpDatabase);
   it("should create a user", async done => {
     const res = await request(app)
       .post("/api/user/v1/")
-      .send(jsonData);
+      .send(testJsonData);
+    testUserID = res.header.location.split("=")[1];
+    testJsonDataResponse.id = parseInt(testUserID);
     expect(res.statusCode).toEqual(201);
     expect(res.body).toHaveProperty("response");
-    expect(res.body.response).toBe(
-      "User Created!"
-    );
+    expect(res.body.response).toBe("user created");
     done();
   });
+  it("should return all users", async done => {
+    const res = await request(app).get(`/api/user/v1/`);
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty("response");
+    expect(res.body.response).toStrictEqual([testJsonDataResponse]);
+    done();
+  });
+  it("should return one json file of a user", async done => {
+    const res = await request(app).get(`/api/user/v1?id=${testUserID}`);
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty("response");
+    expect(res.body.response).toStrictEqual(testJsonDataResponse);
+    done();
+  });
+  it("should update fields of the user model based on keys in the request json file", async done => {
+    const res = await request(app)
+      .put(`/api/user/v1/`)
+      .send({
+        id: testUserID,
+        user: {
+          email: "newTestUser001@gmail.com",
+          firstName: "nameLast",
+          lastName: "nameFirst"
+        }
+      });
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty("response");
+    expect(res.body.response).toBe("user info updated");
+    done();
+  });
+  // TODO: Add PUT test case for roles changes
+  it("should delete one user", async done => {
+    const res = await request(app)
+      .delete(`/api/user/v1`)
+      .send({ id: [testUserID] });
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty("response");
+    expect(res.body.response).toBe("users deleted");
+    done();
+  });
+  // TODO: Add DELETE test case for deleting single use w/ no array
 });
