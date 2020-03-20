4774690e205bbebc7c6ba9f8d11023000a42fd6a
Alex Mendez <a.mendez@outlook.com>
2020-03-20 16:38:20 -0400

    Created crud api for rol model

--- controllers/router.js
+++ controllers/router.js
@@ -4,4 +4,6 @@ const { router: Auth, version: AuthVersion } = require("./Authentication");
 router.use(`/auth/v${AuthVersion}`, Auth);
 const { router: User, version: UserVersion } = require("./user");
 router.use(`/user/v${UserVersion}`, User);
+const { router: Role, version: RoleVersion } = require("./role");
+router.use(`/role/v${RoleVersion}`, Role);
 module.exports = router;
