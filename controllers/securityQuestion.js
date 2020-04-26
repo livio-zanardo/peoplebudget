4715a3b3dae8f323c15309431fe61762d7eba4f5
Earl Cameron <mr.e.cameron@gmail.com>
2020-04-26 18:13:07 -0400

    fixed validation logic

--- controllers/securityQuestion.js
+++ controllers/securityQuestion.js
@@ -1,26 +1,18 @@
 const securityQuestion = require("../models/securityQuestion");
-​
 const router = require("express").Router();
-​
 const { hash } = require("../helpers/hash");
 const { customValidator } = require("../helpers/validator");
 const { alreadyExists } = require("../helpers/database");
 const { ClientError, ServerError } = require("../helpers/error");
 const pagination = require("../helpers/pagination");
-​
 let results;
-​
-// Client -> API -> Databaese
+
 router.post("/", async (req, res, next) => {
-  console.log(req.body.question);
   results = await securityQuestion.create({
     question: req.body.question
   });
 });
-​
-// Client <- API <- Databaese
 router.get("/", async (req, res, next) => {
-  //let results;
   try {
     if (req.query.hasOwnProperty("id")) {
       results = await securityQuestion.findOne({
@@ -37,7 +29,6 @@ router.get("/", async (req, res, next) => {
   }
 });
 router.put(   "/", async (req, res, next) => {
-    console.log(req.body.securityquestion);
     results = await securityQuestion.update(
         {question: req.body.securityquestion},
         {
@@ -47,7 +38,6 @@ router.put(   "/", async (req, res, next) => {
     res.send("hello");
 });
 router.delete("/", async (req, res, next) => {
-​
 });
 ​
 module.exports = { router, version: 1 };
