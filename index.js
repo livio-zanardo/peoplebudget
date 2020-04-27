5c0a204a91c6587aa479bf5be2bdd14bc0c71e73
Earl Cameron <mr.e.cameron@gmail.com>
2020-04-26 20:53:10 -0400

    linted

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const server = require("./app/app");
+const server = require('./app/app');
 server.listen(process.env.PORT, (message) =>
-  console.log(`app listening on post:${process.env.PORT}`)
+    console.log(`App listening on port:${process.env.PORT}`)
 );
