675fc22fa030814d17312ee7a7f143caa53973ba
Earl Cameron <mr.e.cameron@gmail.com>
2020-05-03 00:02:15 -0400

    init

--- tests/comment.test.js
+++ tests/comment.test.js
@@ -1,84 +1,80 @@
-const request = require("supertest");
-const mysql = require("mysql2/promise");
-const app = require("../app");
-const comment = require("../models/comment");
+const request = require('supertest');
+const mysql = require('mysql2/promise');
+const app = require('../app/app');
+// const comment = require('../models/comment');
 
-const cleaningUpDatabase = async done => {
-  console.log("Deleting all test data...");
-  const connection = await mysql.createConnection({
-    host: process.env.DBHOST,
-    user: process.env.DBROOTUSER,
-    password: process.env.DBROOTPW,
-    database: `${process.env.DB}_test`
-  });
-  await connection.execute(`DELETE FROM comments`);
-  await connection.end();
-  done();
+const cleaningUpDatabase = async (done) => {
+    console.log('Deleting all test data...');
+    const connection = await mysql.createConnection({
+        host: process.env.DBHOST,
+        user: process.env.DBROOTUSER,
+        password: process.env.DBROOTPW,
+        database: `${process.env.DB}_test`
+    });
+    await connection.execute(`DELETE FROM comments`);
+    await connection.end();
+    done();
 };
 let testCommentsID = null;
 const testJsonData = {
-  userid: 1,
-  postid: 1,
-  commentBody: "Hello"
+    userid: 1,
+    postid: 1,
+    commentBody: 'Hello'
 };
 const testJsonDataResponse = {
-  id: "",
-  userid: 1,
-  postid: 1,
-  commentBody: "Hello"
+    id: '',
+    userid: 1,
+    postid: 1,
+    commentBody: 'Hello'
 };
-describe("Comments CRUD API", () => {
-  beforeAll(cleaningUpDatabase);
-  it("should create a comment", async done => {
-    const res = await request(app)
-      .post("/api/comment/v1/")
-      .send(testJsonData);
-    testCommentsID = parseInt(res.header.location.split("=")[1]);
-    testJsonDataResponse.id = testCommentsID;
-    expect(res.statusCode).toEqual(201);
-    expect(res.body).toHaveProperty("response");
-    expect(res.body.response).toBe("comment created");
-    done();
-  });
-  it("should return all comments", async done => {
-    const res = await request(app).get(`/api/comment/v1/`);
-    expect(res.statusCode).toEqual(200);
-    expect(res.body).toHaveProperty("response");
-    expect(res.body.response.rows).toStrictEqual([testJsonDataResponse]);
-    done();
-  }); //Worked on
-  it("should return one comment", async done => {
-    const res = await request(app).get(`/api/comment/v1?id=${testCommentsID}`);
-    expect(res.statusCode).toEqual(200);
-    expect(res.body).toHaveProperty("response");
-    expect(res.body.response).toStrictEqual(testJsonDataResponse);
-    done();
-  });
-  it("should update fields of the user model based on keys in the request json file", async done => {
-    const res = await request(app)
-      .put(`/api/user/v1/`)
-      .send({
-        id: testCommentsID,
-        user: {
-          email: "newTestUser001@gmail.com",
-          firstName: "nameLast",
-          lastName: "nameFirst"
-        }
-      });
-    expect(res.statusCode).toEqual(200);
-    expect(res.body).toHaveProperty("response");
-    expect(res.body.response).toBe("user info updated");
-    done();
-  });
-  // TODO: Add PUT test case for roles changes
-  it("should delete one user", async done => {
-    const res = await request(app)
-      .delete(`/api/user/v1`)
-      .send({ id: [testCommentsID] });
-    expect(res.statusCode).toEqual(200);
-    expect(res.body).toHaveProperty("response");
-    expect(res.body.response).toBe("users deleted");
-    done();
-  });
-  // TODO: Add DELETE test case for deleting single use w/ no array
+describe('Comments CRUD API', () => {
+    beforeAll(cleaningUpDatabase);
+    it('should create a comment', async (done) => {
+        const res = await request(app).post('/api/comment/v1/').send(testJsonData);
+        testCommentsID = parseInt(res.header.location.split('=')[1]);
+        testJsonDataResponse.id = testCommentsID;
+        expect(res.statusCode).toEqual(201);
+        expect(res.body).toHaveProperty('response');
+        expect(res.body.response).toBe('comment created');
+        done();
+    });
+    it('should return all comments', async (done) => {
+        const res = await request(app).get(`/api/comment/v1/`);
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toHaveProperty('response');
+        expect(res.body.response.rows).toStrictEqual([testJsonDataResponse]);
+        done();
+    });
+    it('should return one comment', async (done) => {
+        const res = await request(app).get(`/api/comment/v1?id=${testCommentsID}`);
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toHaveProperty('response');
+        expect(res.body.response).toStrictEqual(testJsonDataResponse);
+        done();
+    });
+    it('should update fields of the user model based on keys in the request json file', async (done) => {
+        const res = await request(app)
+            .put(`/api/user/v1/`)
+            .send({
+                id: testCommentsID,
+                user: {
+                    email: 'newTestUser001@gmail.com',
+                    firstName: 'nameLast',
+                    lastName: 'nameFirst'
+                }
+            });
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toHaveProperty('response');
+        expect(res.body.response).toBe('user info updated');
+        done();
+    });
+    it('should delete one user', async (done) => {
+        const res = await request(app)
+            .delete(`/api/user/v1`)
+            .send({ id: [testCommentsID] });
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toHaveProperty('response');
+        expect(res.body.response).toBe('users deleted');
+        done();
+    });
 });
--- tests/postFollow.test.js
+++ tests/postFollow.test.js
@@ -1,10 +1,10 @@
-const request = require("supertest");
-const mysql = require("mysql2/promise");
-const app = require("../app");
-const postfollow = require("../models/postfollow");
+const request = require('supertest');
+const mysql = require('mysql2/promise');
+const app = require('../app');
+// const postfollow = require('../models/postfollow');
 
 const cleaningUpDatabase = async (done) => {
-    console.log("Deleting all test data...");
+    console.log('Deleting all test data...');
     const connection = await mysql.createConnection({
         host: process.env.DBHOST,
         user: process.env.DBROOTUSER,
@@ -16,13 +16,13 @@ const cleaningUpDatabase = async (done) => {
     done();
 };
 
-let testFollowedPostId = 1;
+const testFollowedPostId = 1;
 const testJsonData = {
     followedPostId: 1,
     followingUserId: 2
 };
 const testJsonDataResponse = {
-    id: "",
+    id: '',
     followedPostId: 1,
     followingUserId: 2
 };
@@ -30,36 +30,32 @@ const testJsonDataResponsePostError = {
     followingUserId: 2
 };
 
-describe("postFollow CRUD API", () => {
+describe('postFollow CRUD API', () => {
     beforeAll(cleaningUpDatabase);
-    it("should create a postFollowed", async (done) => {
-        const res = await request(app)
-            .post("/api/postfollow/v1/")
-            .send(testJsonData);
-        testfollowingUserID = res.header.location.split("=")[1];
+    it('should create a postFollowed', async (done) => {
+        const res = await request(app).post('/api/postfollow/v1/').send(testJsonData);
+        testfollowingUserID = res.header.location.split('=')[1];
         testJsonDataResponse.id = parseInt(testfollowingUserID);
 
         expect(res.statusCode).toEqual(201);
-        expect(res.body).toHaveProperty("response");
-        expect(res.body.response).toBe("postfollow created");
+        expect(res.body).toHaveProperty('response');
+        expect(res.body.response).toBe('postfollow created');
         done();
     });
-    it("should return an error with missing followedPostId", async (done) => {
+    it('should return an error with missing followedPostId', async (done) => {
         const res = await request(app)
-            .post("/api/postfollow/v1/")
+            .post('/api/postfollow/v1/')
             .send(testJsonDataResponsePostError);
 
         expect(res.statusCode).toEqual(400);
-        expect(res.body).toHaveProperty("ClientError");
-        expect(res.body.ClientError).toBe(
-            "Missing paramater 'followedPostId' ."
-        );
+        expect(res.body).toHaveProperty('ClientError');
+        expect(res.body.ClientError).toBe("Missing paramater 'followedPostId' .");
         done();
     });
-    it("should return all the followed posts", async (done) => {
+    it('should return all the followed posts', async (done) => {
         const res = await request(app).get(`/api/postfollow/v1/`);
         expect(res.statusCode).toEqual(200);
-        expect(res.body).toHaveProperty("response");
+        expect(res.body).toHaveProperty('response');
         expect(res.body.response).toStrictEqual({
             count: 1,
             maxPages: 1,
@@ -73,57 +69,45 @@ describe("postFollow CRUD API", () => {
         });
         done();
     });
-    it("should give an error for id not in database", async (done) => {
-        const res = await request(app).get(
-            `/api/postfollow/v1/?id=${testFollowedPostId}`
-        );
+    it('should give an error for id not in database', async (done) => {
+        const res = await request(app).get(`/api/postfollow/v1/?id=${testFollowedPostId}`);
         expect(res.statusCode).toEqual(400);
-        expect(res.body).toHaveProperty("ClientError");
-        expect(res.body.ClientError).toBe(
-            `id '${testFollowedPostId}' doesn't exist`
-        );
+        expect(res.body).toHaveProperty('ClientError');
+        expect(res.body.ClientError).toBe(`id '${testFollowedPostId}' doesn't exist`);
         done();
     });
-    it("should update followedPostId", async (done) => {
+    it('should update followedPostId', async (done) => {
         const res = await request(app).put(`/api/postfollow/v1/`).send({
             id: testfollowingUserID,
-            followedPostId: "5"
+            followedPostId: '5'
         });
         expect(res.statusCode).toEqual(200);
-        expect(res.body).toHaveProperty("response");
-        expect(res.body.response).toBe("follow info updated");
+        expect(res.body).toHaveProperty('response');
+        expect(res.body.response).toBe('follow info updated');
         done();
     });
-    it("should give an error missing id parameter", async (done) => {
-        const res = await request(app)
-            .put("/api/postfollow/v1/")
-            .send(testJsonData);
+    it('should give an error missing id parameter', async (done) => {
+        const res = await request(app).put('/api/postfollow/v1/').send(testJsonData);
         expect(res.statusCode).toEqual(400);
-        expect(res.body).toHaveProperty("ClientError");
+        expect(res.body).toHaveProperty('ClientError');
         expect(res.body.ClientError).toBe("Missing paramater 'id' .");
         done();
     });
-    it("should delete one userfollow", async (done) => {
+    it('should delete one userfollow', async (done) => {
         const res = await request(app)
             .delete(`/api/postfollow/v1`)
             .send({ id: testfollowingUserID });
         expect(res.statusCode).toEqual(200);
-        expect(res.body).toHaveProperty("response");
-        expect(res.body.response).toBe(
-            `Id '${testfollowingUserID}' was deleted`
-        );
+        expect(res.body).toHaveProperty('response');
+        expect(res.body.response).toBe(`Id '${testfollowingUserID}' was deleted`);
         done();
     });
-    it("should give an error missing id parameter", async (done) => {
-        const res = await request(app)
-            .delete("/api/postfollow/v1/")
-            .send(testJsonDataResponse);
+    it('should give an error missing id parameter', async (done) => {
+        const res = await request(app).delete('/api/postfollow/v1/').send(testJsonDataResponse);
 
         expect(res.statusCode).toEqual(400);
-        expect(res.body).toHaveProperty("ClientError");
-        expect(res.body.ClientError).toBe(
-            `id '${testJsonDataResponse.id}' doesn't exist`
-        );
+        expect(res.body).toHaveProperty('ClientError');
+        expect(res.body.ClientError).toBe(`id '${testJsonDataResponse.id}' doesn't exist`);
         done();
     });
 });
