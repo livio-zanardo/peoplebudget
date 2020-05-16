d1bbf40d90162ed8ef5dfb8c6fbfc0b386176219
Osmany Pujol <Osmany.Pujolcs@gmail.com>
2020-05-15 21:17:04 -0400

    updated the button component

--- view/src/components/Button/index.js
+++ view/src/components/Button/index.js
@@ -4,7 +4,7 @@ import Style from './index.module.css';
 const Button = (props) => {
     return (
         <button className={`${Style.Button} ${Style.buttonShadow} `} onClick={props.onClick}>
-            { props.children ? props.children : 'Login'}
+            { props.children ? props.children : 'Default'}
         </button>
     );
 };
