41972b4214a3580ac68fffc6e43c8399efb57d4c
Miguel Amaro <miguel.amaro001@mymdc.net>
2020-05-18 20:10:03 -0400

    cleaned up logic for input react component

--- view/src/components/Home.js
+++ view/src/components/Home.js
@@ -1,28 +1,9 @@
 import React, { useState, useRef } from 'react';
-import Input from './Input/index';
 
 const Home = () => {
-    const [value, setValue] = useState('');
-    const nameForm = useRef(null);
-    /*
-    const handleSubmit = (event) => {
-        alert('An essay was submitted: ' + value);
-        event.preventDefault();
-        console.log(value.term);
-    };
-    */
-    const handleClickEvent = () => {
-        const form = nameForm.current;
-        alert(`${form['Pmail'].value} ${form['Password'].value}`);
-    };
     return (
         <div>
             <h1>Home Page</h1>
-            <form ref={nameForm}>
-                <Input name={'Email'} label="Email" placeHolder={'Email'} type={'text'} />
-                <Input name={'Password'} placeHolder={'Password'} type={'email'} />
-            </form>
-            <button onClick={handleClickEvent}>submit</button>
         </div>
     );
 };
--- view/src/components/Input/index.js
+++ view/src/components/Input/index.js
@@ -1,30 +1,23 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import style from './index.module.css';
-//import idGenerator from '../../helpers/IdGenerator';
 
 const Input = (props) => {
-    // States
-    const [value, setValue] = useState('');
+    const [searchTerm, setSearchTerm] = useState('');
 
-    // Generator
-    //const gen = idGenerator('input', 0);
-    //let id = gen.next().value;
-
-    // Handlers
     const onChangeHandler = (event) => {
-        setValue(event.target.value);
+        setSearchTerm(event.target.value);
     };
 
     return (
         <div className={`${style.container}`}>
-            <label className={`${style.label}`} forHtml={`${props.name}`}>
+            <label className={`${style.label}`} forhtml={`${props.name}`}>
                 {props.label}
             </label>
             <input
                 className={`${style.input}`}
                 name={`${props.name}`}
                 id={`${props.name}`}
-                value={value}
+                value={searchTerm}
                 type={`${props.type}`}
                 placeholder={props.placeHolder}
                 onChange={onChangeHandler}
