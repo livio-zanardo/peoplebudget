51e6687a95aa16665d0a5b634ad52b39345c2c42
aaaragon <aragonandre333@gmail.com>
2020-05-21 20:57:54 -0400

    init

--- view/src/pages/Login.jsx
+++ view/src/pages/Login.jsx
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
+import Input from '../components/Input/index';
+import Button from '../components/Button/index';
+import { useHistory } from 'react-router-dom';
+
 
 const Login = () => {
+
+    const [value, setValue] = useState('');
+    const nameForm = useRef(null);
+
+    let history = useHistory();
+
+    const handleClickEvent = () => {
+        const form = nameForm.current;
+        //alert(`${form['email'].value} ${form['password'].value}`);
+        history.push("/proposals");
+    };
+
     return (
         <div className="row" style={{ height: '100%' }}>
             <div className="col-sm-4 border bg-light p-0">
@@ -12,25 +28,30 @@ const Login = () => {
                 </div>
                 <div className="row">
                     <div className="col-sm-8 border ml-5 mt-5 p-3">
+                    <form ref={nameForm}>
                         <div className="row">
                             <div className="col pb-2">
                                 {' '}
-                                <input className="form-control" type="email" placeholder="email" />
+                                
+                                <Input name = {'email'}
+                                className="form-control" type="email" placeholder="email" />
                             </div>
                         </div>
                         <div className="row">
                             <div className="col pb-2">
                                 {' '}
-                                <input
+                                <Input
+                                    name={'password'}
                                     className="form-control"
                                     type="password"
                                     placeholder="password"
                                 />
                             </div>
                         </div>
+                        </form>
                         <div className="row">
                             <div className="col mr-auto">
-                                <button className="btn btn-block btn-primary">login</button>
+                                <Button className="btn btn-block btn-primary" onClick={handleClickEvent}>login</Button>
                             </div>
                         </div>
                     </div>
