3dbfa90d5547490168ec757a0c77bf6a637b9db1
Osmany Pujol <Osmany.Pujolcs@gmail.com>
2020-05-23 17:41:43 -0400

    Navbar styling

--- view/src/components/Navbar/NavButton/index.jsx
+++ view/src/components/Navbar/NavButton/index.jsx
@@ -5,9 +5,12 @@ import { links } from './index.module.css';
 const NavButton = ({ text, link }) => {
     return (
         <div className={`col-sm-auto text-center text-capitalize`}>
+            <h6 className='p-2 mx-1 font-weight-bold'>
             <Link className={`${links}`} to={link}>
                 {text}
             </Link>
+            </h6>
+            
         </div>
     );
 };
--- view/src/components/Navbar/index.jsx
+++ view/src/components/Navbar/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import NavButton from './NavButton/index';
 
+
 const Navbar = (props) => {
     const mapButtons = (options) =>
         options.map((values, index) => <NavButton key={index} {...values} />);
     return (
         <div className="row">
             <div className="col-sm-auto ml-auto">
-                <div className="row">
+                <div className="row ">
                     {mapButtons(props.options ? props.options : [{ text: 'home', link: '/' }])}
                 </div>
             </div>
