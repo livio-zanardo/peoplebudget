ecda8f23b54af3f1493d48f91a3b4ab4e2c0986e
SashaFeliu <58046474+SashaFeliu@users.noreply.github.com>
2020-05-25 10:55:37 -0400

    Created Footer

---
+++ view/src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const footer = () => {
+    return (
+        <div className="row">
+            <div className="col" style={{ backgroundColor: '#084652', height: '180px', width: '100%'}}></div>
+        </div>
+    );
+};
+
+export default footer;
