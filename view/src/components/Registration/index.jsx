f50c4bb770f4a5b8b677b80dfe0b78e9fc4e0b2e
Miguel Amaro <miguel.amaro001@mymdc.net>
2020-05-26 20:25:41 -0400

    making site more responsive

--- view/src/components/Registration/index.jsx
+++ view/src/components/Registration/index.jsx
@@ -3,36 +3,54 @@ import Card from '../Card/index';
 import { Link } from 'react-router-dom';
 import Button from '../Button/index';
 import mapImg from '../../miamiMap.png';
-import { registration, card } from './index.module.css';
+import { card, imageContainer, container } from './index.module.css';
 
 const Registration = () => {
     return (
-        <div className={`row`} style={{margin: 'auto'}}>
-            <div className='col p-0'>
-                <img className="w-100 h-100" src={mapImg} alt="Map of Miami" />
+        <div className={`row p-0`} style={{ margin: 'auto' }}>
+            <div className={`${imageContainer} col-12 p-0`} style={{ position: 'relative' }}>
+                <img
+                    className="w-100"
+                    style={{ position: 'relative' }}
+                    src={mapImg}
+                    alt="Map of Miami"
+                />
             </div>
-            <Card
-                customClassName={card}
-                top="50%"
-                left="50%"
-                radius="2em"
-                border
-                shadow
-            >
-                <div className="mx-auto p-3" >
-                    <div className="row">
-                        <div className="col text-center">
-                            <h1>People Budget</h1>
-                            <p>
-                                <b>A civic engagement process for educating Miamians on budgeting</b>
-                            </p>
+            <div className={`col-5 ${container} my-auto`} style={{ position: 'relative', height: '0px' }}>
+                .
+                <Card customClassName={`${card}`} left="50vw" sidePadding="4em" centerPadding="4em" radius="2em" border shadow>
+                    <div className="p-3">
+                        <div className="row">
+                            <div className="col text-center">
+                                <h1>People Budget</h1>
+                                <hr style={{ width: '100%', borderTop: '2px solid #ee9623' }} />
+                                <p>
+                                    <b>
+                                        A civic engagement process for educating Miamians on
+                                        budgeting
+                                    </b>
+                                </p>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col text-center">
+                                <Link color="white" to="/register">
+                                    <Button
+                                      
+                                        color="#ee9623"
+                                        radius="5em"
+                                        sidePadding="1em"
+                                        centerPadding=".4em"
+                                        shadow
+                                    >
+                                        Get Started
+                                    </Button>
+                                </Link>
+                            </div>
                         </div>
                     </div>
-                    <Link color="white" to="/register">
-                        <Button>Get Started</Button>
-                    </Link>
-                </div>
-            </Card>
+                </Card>
+            </div>
         </div>
     );
 };
