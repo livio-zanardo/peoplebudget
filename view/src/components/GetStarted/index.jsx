eb1dfac7892ca66384d4f9eefd42c0af4524898e
Jeffrey Yuvero <jeffrey.yuvero001@mymdc.net>
2020-07-04 00:44:43 -0400

    Homepage get started card

--- view/src/components/GetStarted/index.jsx
+++ view/src/components/GetStarted/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Card from '../Card/index';
 import { Link } from 'react-router-dom';
 import Button from '../Button/index';
-import { card, imageContainer, container, wrapper, text } from './index.module.css';
+import { card, imageContainer, wrapper, text, hr, button } from './index.module.css';
 
 const GetStarted = () => {
     return (
@@ -12,25 +12,30 @@ const GetStarted = () => {
                 <Card className={`${card}`} radius="2em">
                     <div className={`${text}`}>
                         <h1>People Budget</h1>
-                        <hr style={{ width: '100%', borderTop: '2px solid #ee9623' }} />
+                        <hr className={`${hr}`} />
                         <p>
                             <b>
                                 A civic engagement process for educating Miamians on
                                 budgeting
                             </b>
                         </p>
-                        <Link color="white" to="/register">
-                            <Button
+                        <div className={`${button}`}>
+                            <Link color="white" to="/register">
+                                <Button
+                                    width="65%"
+                                    color="#ee9623"
+                                    radius="5em"
+                                    sidePadding="1em"
+                                    centerPadding=".4em"
+                                    shadow
+                                >
+                                    Get Started
+                                </Button>
+                            </Link>
 
-                                color="#ee9623"
-                                radius="5em"
-                                sidePadding="1em"
-                                centerPadding=".4em"
-                                shadow
-                            >
-                            Get Started
-                            </Button>
-                        </Link>
+                        </div>
+
+
                     </div>
                 </Card>
             </div>
