0602f90b074c5ea35c62da08ae383591b6be304b
Miguel Amaro <miguel.amaro001@mymdc.net>
2020-07-05 17:16:30 -0400

    removed inline styles

--- view/src/components/Button/index.jsx
+++ view/src/components/Button/index.jsx
@@ -1,41 +1,11 @@
 import React from 'react';
-import style, { button } from './index.module.css';
+import { button } from './index.module.css';
 
-const Button = ({
-    onClick,
-    children,
-    color = 'white',
-    radius = '0',
-    width = 'fit-content',
-    height = 'fit-content',
-    sidePadding = '0',
-    centerPadding = '0',
-    top = '0',
-    bottom = '0',
-    left = '0',
-    right = '0',
-    shadow,
-    border,
-    customClassName
-}) => {
+const Button = ({ children, onClick, customClassName }) => {
     return (
         <button
-            className={`${button} ${shadow ? style.buttonShadow : ''}
-        ${border ? 'border' : ''} ${customClassName ? customClassName : ''}`}
-            style={{
-                backgroundColor: color,
-                width: width,
-                height: height,
-                paddingLeft: sidePadding,
-                paddingRight: sidePadding,
-                paddingTop: centerPadding,
-                paddingBottom: centerPadding,
-                borderRadius: radius,
-                top: top,
-                bottom: bottom,
-                left: left,
-                right: right
-            }}
+            className={`${button} 
+        ${customClassName ? customClassName : ''}`}
             onClick={onClick}
         >
             {children ? children : 'Default'}
