349f76d5322c20114d8125354f136fc5c6d464be
Earl Cameron <mr.e.cameron@gmail.com>
2020-07-16 23:55:35 -0400

    slight refactor, removed card class

--- view/src/components/GetStarted/index.jsx
+++ view/src/components/GetStarted/index.jsx
@@ -1,23 +1,18 @@
 import React from 'react';
-import Card from '../Card/index';
 import { Link } from 'react-router-dom';
 import Button from '../Button/index';
 import { card, imageContainer, wrapper, text, hr, button } from './index.module.css';
 
 const GetStarted = () => {
     return (
-
         <div className={`${imageContainer}`}>
             <div className={`${wrapper}`}>
-                <Card className={`${card}`} radius="2em">
-                    <div className={`${text}`}>
+                <div>
+                    <div>
                         <h1>People Budget</h1>
-                        <hr className={`${hr}`} />
+                        <hr />
                         <p>
-                            <b>
-                                A civic engagement process for educating Miamians on
-                                budgeting
-                            </b>
+                            <b>A civic engagement process for educating Miamians on budgeting</b>
                         </p>
                         <div className={`${button}`}>
                             <Link color="white" to="/register">
@@ -32,14 +27,10 @@ const GetStarted = () => {
                                     Get Started
                                 </Button>
                             </Link>
-
                         </div>
-
-
                     </div>
-                </Card>
+                </div>
             </div>
-
         </div>
     );
 };
